1a18181bb8a92a73e3c38035842c6611b06ea409
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-12-17 15:56:06 +0100

    Add core_bench

---
+++ bench/bench_with_core.ml
@@ -0,0 +1,127 @@
+open Core
+open Core_bench
+
+let random ln =
+  let open Stdlib in
+  let open Pervasives in
+  let ic = open_in "/dev/urandom" in
+  let rs = Bytes.create ln in
+  really_input ic rs 0 ln ;
+  close_in ic ;
+  Bytes.unsafe_to_string rs
+
+let push_fke n =
+  let raw = random n in
+  let data = List.init n ~f:(String.get raw) in
+  Staged.stage (fun () -> List.fold_left ~f:Ke.Fke.push ~init:Ke.Fke.empty data)
+
+let push_rke n =
+  let queue = Ke.Rke.create ~capacity:n () in
+  let raw = random n in
+  Staged.stage (fun () -> String.iter ~f:(Ke.Rke.push queue) raw)
+
+let push_mke n =
+  let queue = Ke.Mke.create ~capacity:n () in
+  let raw = random n in
+  Staged.stage (fun () -> String.iter ~f:(Ke.Mke.push queue) raw)
+
+let push_rke_n n =
+  let queue = Ke.Rke.create ~capacity:n () in
+  let raw = random n in
+  let blit src src_off dst dst_off len = Bigstringaf.unsafe_blit_from_string src ~src_off dst ~dst_off ~len in
+  Staged.stage (fun () -> Ke.Rke.N.push queue ~blit ~length:String.length raw)
+
+let push_queue n =
+  let queue = Queue.create () in
+  let raw = random n in
+  Staged.stage (fun () -> String.iter ~f:(fun chr -> Queue.enqueue queue chr) raw)
+
+let push_and_pop_fke n =
+  let raw = random n in
+  let data = List.init n ~f:(String.get raw) in
+  Staged.stage (fun () ->
+      let q = List.fold_left ~f:Ke.Fke.push ~init:Ke.Fke.empty data in
+      let rec go q =
+        if not (Ke.Fke.is_empty q) then
+          let _, q = Ke.Fke.pop_exn q in
+          go q
+        else () in
+      go q)
+
+let push_and_pop_mke n =
+  let queue = Ke.Mke.create ~capacity:n () in
+  let raw = random n in
+  Staged.stage (fun () ->
+      String.iter ~f:(Ke.Mke.push queue) raw ;
+      while not (Ke.Mke.is_empty queue) do ignore (Ke.Mke.pop queue) done)
+
+let push_and_pop_rke n =
+  let queue = Ke.Rke.create ~capacity:n () in
+  let raw = random n in
+  Staged.stage (fun () ->
+      String.iter ~f:(Ke.Rke.push queue) raw ;
+      while not (Ke.Rke.is_empty queue) do ignore (Ke.Rke.pop queue) done)
+
+let push_and_pop_queue n =
+  let queue = Queue.create () in
+  let raw = random n in
+  Staged.stage (fun () ->
+      String.iter ~f:(fun chr -> Queue.enqueue queue chr) raw ;
+      while not (Queue.is_empty queue) do ignore (Queue.dequeue_exn queue) done)
+
+let test_push_fke =
+  Test.create_indexed ~name:"Fke.push"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_fke
+
+let test_push_mke =
+  Test.create_indexed ~name:"Mke.push"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_mke
+
+let test_push_rke =
+  Test.create_indexed ~name:"Rke.push"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_rke
+
+let test_push_rke_n =
+  Test.create_indexed ~name:"Rke.N.push"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_rke_n
+
+let test_push_queue =
+  Test.create_indexed ~name:"Queue.push"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_queue
+
+let tests_push = [ test_push_fke; test_push_rke; test_push_rke_n; test_push_queue; test_push_mke ]
+
+let test_push_and_pop_fke =
+  Test.create_indexed ~name:"Fke.push & Fke.pop"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_and_pop_fke
+
+let test_push_and_pop_mke =
+  Test.create_indexed ~name:"Mke.push & Push.pop"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_and_pop_mke
+
+let test_push_and_pop_rke =
+  Test.create_indexed ~name:"Rke.push & Rke.pop"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_and_pop_rke
+
+let test_push_and_pop_queue =
+  Test.create_indexed ~name:"Queue.push & Queue.pop"
+    ~args:[100; 500; 1000; 5000; 10000;]
+    push_and_pop_queue
+
+let tests_push_and_pop =
+  [ test_push_and_pop_fke
+  ; test_push_and_pop_rke
+  ; test_push_and_pop_queue
+  ; test_push_and_pop_mke ]
+
+let command  = Bench.make_command (tests_push @ tests_push_and_pop)
+
+let () = Command.run command
---
+++ bench/bfs.ml
@@ -0,0 +1,79 @@
+module type F_PROBLEM = sig
+  type state
+  type move
+
+  val success : state -> bool
+  val moves : state -> (move * state) list
+
+  type table
+
+  val create : unit -> table
+  val add : table -> state -> unit
+  val mem : table -> state -> bool
+  val clear : table -> unit
+end
+
+module type M_PROBLEM = sig
+  type move
+
+  val success : unit -> bool
+  val moves : unit -> move list
+  val do_move : move -> unit
+  val undo_move : move -> unit
+  val add : unit -> unit
+  val mem : unit -> bool
+  val clear : unit -> unit
+end
+
+module F (Q : Ke.Sigs.M) (P : F_PROBLEM) = struct
+  let search s0 =
+    let visited = P.create () in
+    let already s = P.mem visited s || (P.add visited s ; false) in
+    let _ = already s0 in
+    let q = Q.create () in
+    Q.push q ([], s0) ;
+    let rec bfs () =
+      if Q.is_empty q then raise Not_found ;
+      let path, s = Q.pop_exn q in
+      if P.success s then s, List.rev path
+      else ( List.iter (fun (m, s') -> if not (already s') then Q.push q (m :: path, s')) (P.moves s) ; bfs () )
+    in bfs ()
+end
+
+module M (Q : Ke.Sigs.M) (P : M_PROBLEM) = struct
+  let rec cut_head n l = if n == 0 then l else cut_head (pred n) (List.tl l)
+
+  let common_psuffix (n1, l1) (n2, l2) =
+    let rec suffix l1 l2 =
+      if l1 == l2 then l1 else suffix (List.tl l1) (List.tl l2) in
+    if n1 < n2 then suffix l1 (cut_head (n2 - n1) l2)
+    else if n2 < n1 then suffix (cut_head (n1 - n2) l1) l2
+    else suffix l1 l2
+
+  let search () =
+    let already () = P.mem () || (P.add () ; false) in
+    let q = Q.create () in
+    Q.push q (0, []) ;
+    let cpath = ref (0, []) in
+    let rec restore_state path =
+      let suf = common_psuffix path !cpath in
+      let rec backward = function
+        | (m :: r) as p when p != suf -> P.undo_move m ; backward r
+        | _ -> () in
+      let rec forward = function
+        | (m :: r) as p when p != suf -> forward r ; P.do_move m
+        | _ -> () in
+      backward (snd !cpath) ;
+      forward (snd path) ;
+      cpath := path in
+    let rec bfs () =
+      if Q.is_empty q then raise Not_found ;
+      let (n, path) as s = Q.pop_exn q in
+      restore_state s ;
+      if P.success ()
+      then List.rev path
+      else if not (already ())
+      then ( List.iter (fun m -> Q.push q (succ n, m :: path)) (P.moves ()) ; bfs () )
+      else bfs () in
+    bfs ()
+end
