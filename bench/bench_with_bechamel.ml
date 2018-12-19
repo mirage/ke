683da5941dfa5177d2e994c6677ad891ee01cbe3
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-12-19 14:20:37 +0100

    Update printer of bechamel results

--- bench/bench_with_bechamel.ml
+++ bench/bench_with_bechamel.ml
@@ -11,8 +11,7 @@ module Realtime_clock = struct
   let make () = ()
   let label () = "realtime-clock"
   let epsilon () = {contents= 0L}
-  let blit () v =
-    v := Oclock.gettime Oclock.realtime
+  let blit () v = v := Oclock.gettime Oclock.realtime
   let diff a b = {contents= Int64.sub !b !a}
   let float x = Int64.to_float !x
 end
@@ -26,15 +25,14 @@ end
 module Instance = struct
   include Instance
 
-  let realtime_clock = Measure.instance (module Realtime_clock) Extension.realtime_clock
+  let realtime_clock =
+    Measure.instance (module Realtime_clock) Extension.realtime_clock
 end
 
 let random ln =
   let ic = open_in "/dev/urandom" in
   let rs = Bytes.create ln in
-  really_input ic rs 0 ln ;
-  close_in ic ;
-  Bytes.unsafe_to_string rs
+  really_input ic rs 0 ln ; close_in ic ; Bytes.unsafe_to_string rs
 
 let push_fke n =
   let raw = random n in
@@ -54,7 +52,9 @@ let push_mke n =
 let push_rke_n n =
   let queue = Ke.Rke.create ~capacity:n Bigarray.Char in
   let raw = random n in
-  let blit src src_off dst dst_off len = Bigstringaf.unsafe_blit_from_string src ~src_off dst ~dst_off ~len in
+  let blit src src_off dst dst_off len =
+    Bigstringaf.unsafe_blit_from_string src ~src_off dst ~dst_off ~len
+  in
   Staged.stage (fun () -> Ke.Rke.N.push queue ~blit ~length:String.length raw)
 
 let push_queue n =
@@ -71,132 +71,135 @@ let push_and_pop_fke n =
         if not (Ke.Fke.is_empty q) then
           let _, q = Ke.Fke.pop_exn q in
           go q
-        else () in
-      go q)
+        else ()
+      in
+      go q )
 
 let push_and_pop_mke n =
   let queue = Ke.Mke.create ~capacity:n () in
   let raw = random n in
   Staged.stage (fun () ->
       String.iter (Ke.Mke.push queue) raw ;
-      while not (Ke.Mke.is_empty queue) do ignore (Ke.Mke.pop queue) done)
+      while not (Ke.Mke.is_empty queue) do
+        ignore (Ke.Mke.pop queue)
+      done )
 
 let push_and_pop_rke n =
   let queue = Ke.Rke.create ~capacity:n Bigarray.Char in
   let raw = random n in
   Staged.stage (fun () ->
       String.iter (Ke.Rke.push queue) raw ;
-      while not (Ke.Rke.is_empty queue) do ignore (Ke.Rke.pop queue) done)
+      while not (Ke.Rke.is_empty queue) do
+        ignore (Ke.Rke.pop queue)
+      done )
 
 let push_and_pop_queue n =
   let queue = Queue.create () in
   let raw = random n in
   Staged.stage (fun () ->
       String.iter (fun chr -> Queue.add chr queue) raw ;
-      while not (Queue.is_empty queue) do ignore (Queue.pop queue) done)
+      while not (Queue.is_empty queue) do
+        ignore (Queue.pop queue)
+      done )
 
 let test_push_fke =
   Test.make_indexed ~name:"Fke.push"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_fke
 
 let test_push_mke =
   Test.make_indexed ~name:"Mke.push"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_mke
 
 let test_push_rke =
   Test.make_indexed ~name:"Rke.push"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_rke
 
 let test_push_rke_n =
   Test.make_indexed ~name:"Rke.N.push"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_rke_n
 
 let test_push_queue =
   Test.make_indexed ~name:"Queue.push"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_queue
 
 let tests_push =
-  [ test_push_fke
-  ; test_push_rke
-  ; test_push_rke_n
-  ; test_push_queue
+  [ test_push_fke; test_push_rke; test_push_rke_n; test_push_queue
   ; test_push_mke ]
 
 let big_push_fke n =
-  Staged.stage @@ fun () ->
+  Staged.stage
+  @@ fun () ->
   let q = ref Ke.Fke.empty in
-  for i = 1 to n do q := Ke.Fke.push !q i done
+  for i = 1 to n do
+    q := Ke.Fke.push !q i
+  done
 
 let big_push_rke n =
-  Staged.stage @@ fun () ->
+  Staged.stage
+  @@ fun () ->
   let q = Ke.Rke.create ~capacity:n Bigarray.Char in
-  for i = 1 to n do Ke.Rke.push q (Obj.magic i) done
+  for i = 1 to n do
+    Ke.Rke.push q (Obj.magic i)
+  done
 
 let big_push_mke n =
-  Staged.stage @@ fun () ->
+  Staged.stage
+  @@ fun () ->
   let q = Ke.Mke.create ~capacity:n () in
-  for i = 1 to n do Ke.Mke.push q i done
+  for i = 1 to n do
+    Ke.Mke.push q i
+  done
 
 let big_push_queue n =
-  Staged.stage @@ fun () ->
+  Staged.stage
+  @@ fun () ->
   let q = Queue.create () in
-  for i = 1 to n do Queue.push i q done
+  for i = 1 to n do
+    Queue.push i q
+  done
 
 let test_big_push_fke =
-  Test.make_indexed ~name:"Fke.big_push"
-    ~args:[10; 1_000_000]
-    big_push_fke
+  Test.make_indexed ~name:"Fke.big_push" ~args:[10; 1_000_000] big_push_fke
 
 let test_big_push_rke =
-  Test.make_indexed ~name:"Rke.big_push"
-    ~args:[10; 1_000_000]
-    big_push_rke
+  Test.make_indexed ~name:"Rke.big_push" ~args:[10; 1_000_000] big_push_rke
 
 let test_big_push_mke =
-  Test.make_indexed ~name:"Mke.big_push"
-    ~args:[10; 1_000_000]
-    big_push_mke
+  Test.make_indexed ~name:"Mke.big_push" ~args:[10; 1_000_000] big_push_mke
 
 let test_big_push_queue =
-  Test.make_indexed ~name:"Queue.big_push"
-    ~args:[10; 1_000_000]
-    big_push_queue
+  Test.make_indexed ~name:"Queue.big_push" ~args:[10; 1_000_000] big_push_queue
 
 let tests_big_push =
-  [ test_big_push_fke
-  ; test_big_push_rke
-  ; test_big_push_queue
-  ; test_big_push_mke ]
+  [test_big_push_fke; test_big_push_rke; test_big_push_queue; test_big_push_mke]
 
 let test_push_and_pop_fke =
   Test.make_indexed ~name:"Fke.push & Fke.pop"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_and_pop_fke
 
 let test_push_and_pop_mke =
   Test.make_indexed ~name:"Mke.push & Push.pop"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_and_pop_mke
 
 let test_push_and_pop_rke =
   Test.make_indexed ~name:"Rke.push & Rke.pop"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_and_pop_rke
 
 let test_push_and_pop_queue =
   Test.make_indexed ~name:"Queue.push & Queue.pop"
-    ~args:[100; 500; 1000; 5000; 10000;]
+    ~args:[100; 500; 1000; 5000; 10000]
     push_and_pop_queue
 
 let tests_push_and_pop =
-  [ test_push_and_pop_fke
-  ; test_push_and_pop_rke
-  ; test_push_and_pop_queue
+  [ test_push_and_pop_fke; test_push_and_pop_rke; test_push_and_pop_queue
   ; test_push_and_pop_mke ]
 
 let zip l1 l2 =
@@ -224,7 +227,8 @@ let pp_ols_result ppf result =
         (Analyze.OLS.predictors result)
         Fmt.(styled style_by_r_square (Dump.list float))
         estimates
-        Fmt.(option float) (Analyze.OLS.r_square result)
+        Fmt.(option float)
+        (Analyze.OLS.r_square result)
   | None ->
       Fmt.pf ppf "%a per %a = #unable-to-compute" Label.pp
         (Analyze.OLS.responder result)
@@ -232,28 +236,29 @@ let pp_ols_result ppf result =
         (Analyze.OLS.predictors result)
 
 let pp_ransac_result ppf result =
-  Fmt.pf ppf "%a per %a = %04.04f [error: %04.04f]"
-    Label.pp (Analyze.RANSAC.responder result)
-    Label.pp (Analyze.RANSAC.predictor result)
+  Fmt.pf ppf "%a per %a = %04.04f [error: %04.04f]" Label.pp
+    (Analyze.RANSAC.responder result)
+    Label.pp
+    (Analyze.RANSAC.predictor result)
     (Analyze.RANSAC.mean result)
     (Analyze.RANSAC.error result)
 
 let pad n x =
   if String.length x > n then x else x ^ String.make (n - String.length x) ' '
 
-let pp_ols_results : (string, Analyze.OLS.t) Hashtbl.t Fmt.t = fun ppf ->
-  Hashtbl.iter
-    (fun test_name result ->
-       Fmt.pf ppf "@[<hov>%s = %a@]@\n"
-         (pad 30 @@ test_name)
-         pp_ols_result result )
+let pp_ols_results : (string, Analyze.OLS.t) Hashtbl.t Fmt.t =
+ fun ppf ->
+  Hashtbl.iter (fun test_name result ->
+      Fmt.pf ppf "@[<hov>[ols]%s = %a@]@\n"
+        (pad 30 @@ test_name)
+        pp_ols_result result )
 
-let pp_ransac_results : (string, Analyze.RANSAC.t) Hashtbl.t Fmt.t = fun ppf ->
-  Hashtbl.iter
-    (fun test_name result ->
-       Fmt.pf ppf "@[<hov>%s = %a@]@\n"
-         (pad 30 @@ test_name)
-         pp_ransac_result result )
+let pp_ransac_results : (string, Analyze.RANSAC.t) Hashtbl.t Fmt.t =
+ fun ppf ->
+  Hashtbl.iter (fun test_name result ->
+      Fmt.pf ppf "@[<hov>[ransac]%s = %a@]@\n"
+        (pad 30 @@ test_name)
+        pp_ransac_result result )
 
 let reporter ppf =
   let report src level ~over k msgf =
@@ -282,14 +287,19 @@ let _, _ = setup_logs (Some `Ansi_tty) (Some Logs.Debug)
 
 let unzip =
   let rec go (l1, l2) = function
-    | [] -> List.rev l1, List.rev l2
-    | (x, y) :: r -> go (x :: l1, y :: l2) r in
+    | [] -> (List.rev l1, List.rev l2)
+    | (x, y) :: r -> go (x :: l1, y :: l2) r
+  in
   go ([], [])
 
 let () =
-  let ols = Analyze.ols ~r_square:true ~bootstrap:0 ~predictors:Measure.[|run|] in
+  let ols =
+    Analyze.ols ~r_square:true ~bootstrap:0 ~predictors:Measure.[|run|]
+  in
   let ransac = Analyze.ransac ~filter_outliers:true ~predictor:Measure.run in
-  let instances = Instance.[minor_allocated; major_allocated; cpu_clock; realtime_clock] in
+  let instances =
+    Instance.[minor_allocated; major_allocated; cpu_clock; realtime_clock]
+  in
   let tests =
     match Sys.argv with
     | [|_|] -> []
@@ -299,35 +309,49 @@ let () =
     | [|_; "all"|] -> tests_push @ tests_big_push @ tests_push_and_pop
     | _ -> Fmt.invalid_arg "%s {push|all}" Sys.argv.(1)
   in
-  let measure_and_analyze test =
-    let results =
-      Benchmark.all ~stabilize:true ~quota:(Benchmark.s 2.) ~run:5000 instances test
-    in
-    List.map
-      (fun x -> Analyze.all ols x results)
-      instances,
-    List.map
-      (fun x -> Analyze.all ransac x results)
+  let ols_results = Hashtbl.create (List.length instances) in
+  let ransac_results = Hashtbl.create (List.length instances) in
+  let () =
+    List.iter
+      (fun x -> Hashtbl.add ols_results (Measure.label x) (Hashtbl.create 16))
       instances
   in
-  let ols_results, ransac_results = unzip (List.map measure_and_analyze tests) in
-  List.iter
-    (fun results ->
-  List.iter
-    (fun (instance, result) ->
-      Fmt.pr "%a: @[<v>%a@]\n%!"
-        Label.pp (Measure.label instance)
-        pp_ols_results result)
-      (zip instances results))
+  let () =
+    List.iter
+      (fun x ->
+        Hashtbl.add ransac_results (Measure.label x) (Hashtbl.create 16) )
+      instances
+  in
+  let measure_and_analyze test =
+    let results =
+      Benchmark.all ~stabilize:true ~quota:(Benchmark.s 2.) ~run:5000 instances
+        test
+    in
+    List.iter
+      (fun x ->
+        let r = Analyze.all ols x results in
+        Hashtbl.add
+          (Hashtbl.find ols_results (Measure.label x))
+          (Test.name test) r )
+      instances ;
+    List.iter
+      (fun x ->
+        let r = Analyze.all ransac x results in
+        Hashtbl.add
+          (Hashtbl.find ransac_results (Measure.label x))
+          (Test.name test) r )
+      instances
+  in
+  let () = List.iter measure_and_analyze tests in
+  Hashtbl.iter
+    (fun label results ->
+      Fmt.pr "%a: @[<v>%a@]\n%!" Label.pp label
+        Fmt.(Dump.hashtbl string pp_ols_results)
+        results )
     ols_results ;
-  List.iter
-    (fun results ->
-  List.iter
-    (fun (instance, results) ->
-       Fmt.pr "%a: %a\n%!"
-         Label.pp (Measure.label instance)
-         pp_ransac_results results)
-    (zip instances results))
+  Hashtbl.iter
+    (fun label results ->
+      Fmt.pr "%a: @[<v>%a@]\n%!" Label.pp label
+        Fmt.(Dump.hashtbl string pp_ransac_results)
+        results )
     ransac_results
-
-
